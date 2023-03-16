906fe9dc5cf6c854a2afdffd21f342f50c72058c
성원 <47082317+Ho-rang@users.noreply.github.com>
2023-03-16 17:03:07 +0900

    Create 1_대충 만든 자판.cpp

---
+++ Level_1/1_대충 만든 자판.cpp
@@ -0,0 +1,45 @@
+﻿#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> solution(vector<string> keymap, vector<string> targets)
+{
+	vector<int> answer;
+
+	std::unordered_map<char, int> map;
+	for (int i = 0; i < keymap.size(); i++)
+	{
+		for (int j = 0; j < keymap[i].length(); j++)
+		{
+			auto key = keymap[i][j];
+			if (map[key] == 0)
+			{
+				map[key] = j + 1;
+			}
+			else if(map[key] > j + 1)
+			{
+				map[key] = j + 1;
+			}
+		}
+	}
+
+	for (auto target : targets)
+	{
+		int sum = 0;
+		for (auto a : target)
+		{
+			if (map[a] == 0)
+			{
+				sum = -1;
+				break;
+			}
+			sum += map[a];
+		}
+		answer.push_back(sum);
+	}
+
+
+	return answer;
+}
