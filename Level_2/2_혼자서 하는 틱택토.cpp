0aa3646dedc2200a63b07d6d1d60e1b27bdb4284
Horang <qkrtk5689@gmail.com>
2023-04-10 18:33:08 +0900

    Update 2_혼자서 하는 틱택토.cpp
    틀린 부분 수정

--- Level_2/2_혼자서 하는 틱택토.cpp
+++ Level_2/2_혼자서 하는 틱택토.cpp
@@ -2,49 +2,69 @@
 #include <vector>
 #include <algorithm>
 
-bool checkWin(std::vector<std::string>& board, char player)
+int checkWin(std::vector<std::string>& board, char player)
 {
+	int winCount = 0;
 	// 가로
 	for (int i = 0; i < 3; i++)
 		if (player == board[i][0] && board[i][1] == player && board[i][2] == player)
-			return true;
+			winCount++;
 
 	// 세로
 	for (int i = 0; i < 3; i++)
 		if (board[0][i] == player && board[1][i] == player && board[2][i] == player)
-			return true;
+			winCount++;
 
 	// 대각선
 	if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
-		return true;
+		winCount++;
 
 	if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
-		return true;
+		winCount++;
 
-	return false;
+	return winCount;
 }
 
 int solution(std::vector<std::string> board)
 {
-	int a = 0;
-	int b = 0;
+	int O = 0;
+	int X = 0;
 
 	for (auto boa : board)
 	{
 		for (auto c : boa)
 		{
 			if (c == 'O')
-				a++;
+				O++;
 			else if (c == 'X')
-				b++;
+				X++;
 		}
 	}
-	if (a < b)
+	if (O < X)
+		return 0;
+	else if (O - X > 1)
+		return 0;
+	else if (O + X == 9 && O - X != 1)
 		return 0;
 
-	if (checkWin(board, 'O') && checkWin(board, 'X'))
-	{
+	int Owin = checkWin(board, 'O');
+	int Xwin = checkWin(board, 'X');
+
+	if (Owin + Xwin > 2)
 		return 0;
+
+	if (Xwin > 0)
+	{
+		if (Owin > 0)
+			return 0;
+
+		if (O > X)
+			return 0;
+	}
+	else if(Owin > 0)
+	{
+		if (!(O - X == 1))
+			return 0;
 	}
 
 	return 1;
