f5bc9d378b8100c4a273f2ef270815f090ed1ae4
Horang <qkrtk5689@gmail.com>
2023-10-26 14:49:21 +0900

    Create 779. K-th Symbol in Grammar.cpp

---
+++ LeetCode/779. K-th Symbol in Grammar.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+class Solution 
+{
+public:
+	int kthGrammar(int n, int k) 
+	{
+		if (n == 1)
+			return 0;
+
+		int x = kthGrammar(n - 1, (k + 1) / 2);
+
+		if (x == 0)
+		{
+			if (k & 1)
+				return 0;
+			else
+				return 1;
+		}
+		else
+		{
+			if (k & 1)
+				return 1;
+			else
+				return 0;
+		}
+	}
+};
