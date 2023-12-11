e6aa1770c44ea7a3777a9c667d975537a4daabe8
성원 <clover24802@gmail.com>
2023-12-11 10:58:36 +0900

    Create 1287. Element Appearing More Than 25% In Sorted Array.cpp

---
+++ LeetCode/1287. Element Appearing More Than 25% In Sorted Array.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+class Solution
+{
+public:
+	int findSpecialInteger(std::vector<int>& arr)
+	{
+		int quarter = arr.size() / 4;
+
+		for (int i = 0; i < arr.size() - quarter; i++)
+		{
+			if (arr[i] == arr[i + quarter])
+				return arr[i];
+		}
+
+		return 0;
+	}
+};
