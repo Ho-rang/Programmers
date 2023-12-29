426640d5308520eca37b5d571e667003ba5a0656
성원 <clover24802@gmail.com>
2023-12-29 11:41:35 +0900

    Create 2_방문 길이.cpp

---
+++ Level_2/2_방문 길이.cpp
@@ -0,0 +1,70 @@
+#include <string>
+#include <vector>
+
+int solution(std::string dirs)
+{
+	enum
+	{
+		NONE = 0b0000,
+		UP = 0b1000,
+		DOWN = 0b0100,
+		LEFT = 0b0010,
+		RIGHT = 0b0001,
+	};
+
+	int answer = 0;
+
+	char passedRoad[10][10] = { 0 };
+
+	int y = 5;
+	int x = 5;
+	for (const auto& dir : dirs)
+	{
+		int ny = y;
+		int nx = x;
+
+		char flag = 0;
+		char nFlag = 0;
+		switch (dir)
+		{
+			case 'U':
+				ny -= 1;
+				flag = UP;
+				nFlag = DOWN;
+				break;
+			case 'D':
+				ny += 1;
+				flag = DOWN;
+				nFlag = UP;
+				break;
+			case 'L':
+				nx -= 1;
+				flag = LEFT;
+				nFlag = RIGHT;
+				break;
+			case 'R':
+				nx += 1;
+				flag = RIGHT;
+				nFlag = LEFT;
+				break;
+			default:
+				break;
+		}
+
+		if (ny < 0 || ny > 9 ||
+			nx < 0 || nx > 9)
+			continue;
+
+		if ((passedRoad[y][x] & flag) == NONE)
+		{
+			passedRoad[y][x] |= flag;
+			passedRoad[ny][nx] |= nFlag;
+			answer++;
+		}
+
+		y = ny;
+		x = nx;
+	}
+
+	return answer;
+}
