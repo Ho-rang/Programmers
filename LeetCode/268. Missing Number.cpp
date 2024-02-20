cd38e9513a1e0da15689156d3fdd063d9f3d60c0
성원 <clover24802@gmail.com>
2024-02-20 22:41:04 +0900

    Update 268. Missing Number.cpp

--- LeetCode/268. Missing Number.cpp
+++ LeetCode/268. Missing Number.cpp
@@ -1,17 +1,10 @@
-#include <string>
 #include <vector>
-#include <ranges>
-#include <algorithm>
 #include <numeric>
 
 class Solution {
 public:
 	int missingNumber(std::vector<int>& nums)
 	{
-		int result{ static_cast<int>((nums.size() * (nums.size() + 1)) / 2) };
-
-		result -= std::accumulate(nums.begin(), nums.end(), 0);
-
-		return result;
+		return (nums.size() * (nums.size() + 1)) / 2 - std::accumulate(nums.begin(), nums.end(), 0);
 	}
 };
