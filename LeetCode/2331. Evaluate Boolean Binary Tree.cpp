5bd6cc62c339c795d0a7e0352cec9ec481d0b4b2
성원 <clover24802@gmail.com>
2024-05-16 11:55:34 +0900

    Create 2331. Evaluate Boolean Binary Tree.cpp

---
+++ LeetCode/2331. Evaluate Boolean Binary Tree.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+
+class Solution
+{
+public:
+	bool evaluateTree(TreeNode* root)
+	{
+		if (root->val == 0)
+			return false;
+		if (root->val == 1)
+			return true;
+
+		if (root->val == 2)
+			return evaluateTree(root->left) || evaluateTree(root->right);
+		else if (root->val == 3)
+			return evaluateTree(root->left) && evaluateTree(root->right);
+		
+		return true;
+	}
+};
